f372ed7bba68813800e04d24e1010432c1188649
github username <github email address>
2019-06-10 13:41:44 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,15 +1,41 @@
 class StudentsController < ApplicationController
+
   def show
+    @student = Student.find(params[:id])
   end
 
   def new
+    @student = Student.new
+    @schools = School.all
+  end
 
+  def create
+    @student = Student.create(student_params)
+    redirect_to student_path(@student.id)
+  end
+
+  def delete
+    @student = Student.find(params[:id])
+    @school = @student.school
+    @student.delete
+    redirect_to school_path(@school)
   end
 
   def edit
+    @student = Student.find(params[:id])
+    @schools = School.all
   end
 
-  def destroy
-
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+    redirect_to student_path @student
   end
+
+  private
+
+  def student_params
+    params.require(:students).permit(:name, :age, :school_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,27 @@
 Rails.application.routes.draw do
 
-  get '/schools', to: "schools#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #schools index page
+  get '/schools', to: 'schools#index'
+
+  #students new page
+  get '/students/new', to: 'students#new'
+
+  #students create and save new student
+  post '/students', to: 'students#create'
+
+  #student edit
+  get '/students/:id/edit', to: 'students#edit'
+
+  #student update
+  patch '/students/:id', to: 'students#update'
+
+  #schools show page
+  get '/schools/:id', to: 'schools#show', as: 'school'
+
+  #students show page
+  get '/students/:id', to: 'students#show', as: 'student'
+
+  #delete student
+  delete '/students/:id', to: 'students#delete'
+
 end
